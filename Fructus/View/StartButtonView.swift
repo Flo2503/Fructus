a9bf43c68922e0ab757eeea626f0ce32bd67a62c
Flo2503 <bruel.florent@gmail.com>
2022-11-05 17:56:30 +0100

    Implemented fruits card

---
+++ Fructus/View/StartButtonView.swift
@@ -0,0 +1,23 @@
+//
+//  StartButtonView.swift
+//  Fructus
+//
+//  Created by Florent on 05/11/2022.
+//
+
+import SwiftUI
+
+struct StartButtonView: View {
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+// MARK: - PREVIEW
+struct StartButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        StartButtonView()
+    }
+}

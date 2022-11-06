031209bfae3b8ba4da6e3fe5318209cb3c625edd
Flo2503 <bruel.florent@gmail.com>
2022-11-06 19:31:00 +0100

    Created blue print data model

---
+++ Fructus/Model/FruitModel.swift
@@ -0,0 +1,19 @@
+//
+//  FruitModel.swift
+//  Fructus
+//
+//  Created by Florent on 06/11/2022.
+//
+
+import SwiftUI
+
+// MARK: - FRUITS DATA MODEL
+
+struct Fruit: Identifiable {
+    var id = UUID()
+    var title: String
+    var headline: String
+    var gradientColors: [Color]
+    var description: String
+    var nutrition: [String]
+}

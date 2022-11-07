f0cc45441a4f2007fce3bde2e8b84bd56f1bef45
Flo2503 <bruel.florent@gmail.com>
2022-11-07 18:56:06 +0100

    Added local data

--- Fructus/Model/FruitModel.swift
+++ Fructus/Model/FruitModel.swift
@@ -13,6 +13,7 @@ struct Fruit: Identifiable {
     var id = UUID()
     var title: String
     var headline: String
+    var image: String
     var gradientColors: [Color]
     var description: String
     var nutrition: [String]

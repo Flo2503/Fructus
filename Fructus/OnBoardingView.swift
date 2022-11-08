06ca4201b68d9937e2ef92ed11430024020f5c57
Flo2503 <bruel.florent@gmail.com>
2022-11-08 19:26:35 +0100

    Created loop displaying all cards on onboarding view

--- Fructus/OnBoardingView.swift
+++ Fructus/OnBoardingView.swift
@@ -10,11 +10,13 @@ import SwiftUI
 struct OnBoardingView: View {
     // MARK: - PROPERTIES
     
+    var fruits: [Fruit] = fruitsData
+        
     // MARK: - BODY
     var body: some View {
         TabView {
-            ForEach(0..<5) { item in
-                FruitCardView()
+            ForEach(fruits[0...5]) { item in
+                FruitCardView(fruit: item)
             }//: LOOP
         }//: TAB
         .tabViewStyle(PageTabViewStyle())
@@ -25,6 +27,6 @@ struct OnBoardingView: View {
 // MARK: - PREVIEW
 struct OnBoardingView_Previews: PreviewProvider {
     static var previews: some View {
-        OnBoardingView()
+        OnBoardingView(fruits: fruitsData)
     }
 }

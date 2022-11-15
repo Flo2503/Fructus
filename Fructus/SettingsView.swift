6a451908cb3bfbe3b7333d1831399e4ae8211435
Flo2503 <bruel.florent@gmail.com>
2022-11-15 19:49:14 +0100

    Added a second section to the settings view. Created a settings row view with optional values in case they are not used

--- Fructus/SettingsView.swift
+++ Fructus/SettingsView.swift
@@ -37,6 +37,17 @@ struct SettingsView: View {
                     // MARK: - SECTION 2
                     
                     // MARK: - SECTION 3
+                    GroupBox(label: SettingsLabelView(labelText: "Application", labelImage: "apps.iphone")) {
+                        SettingsRowView(name: "Developer", content: "John / Jane")
+                        SettingsRowView(name: "Designer", content: "Robert Petras")
+                        SettingsRowView(name: "Compatibility", content: "iOS 14")
+                        SettingsRowView(name: "Web Site", linkLabel: "Swift UI Masterclass", linkDestination: "swiftuimasterclass.com")
+                        SettingsRowView(name: "Twitter", linkLabel: "@RobertPetras", linkDestination: "twitter.com/robertpetras")
+                        SettingsRowView(name: "SwiftUI", content: "2.0")
+                        SettingsRowView(name: "Version", content: "1.1.0")
+
+
+                    }
                     
                 } //: VSTACK
                 .navigationBarTitle(Text("Settings"), displayMode: .large)
